609bf54ebf92ff4c4b8ad7a003166a88dccf8812
fazlay26 <rabboi007@gmail.com>
2022-04-04 00:16:25 +0600

    navbar-done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import DashBoard from './Components/DashBoard/DashBoard';
+import Header from './Components/Header/Header';
+import HomePage from './Components/HomePage/HomePage';
+import NotFound from './Components/NotFound/NotFound';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-5xl'>asifsdfsdf</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<HomePage></HomePage>}></Route>
+        <Route path='/HomePage' element={<HomePage></HomePage>}></Route>
+        <Route path='/Reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/DashBoard' element={<DashBoard></DashBoard>}></Route>
+        <Route path='/Blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/About' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
 
     </div>
   );
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div className='sticky top-0 bg-white px-12 py-8 flex justify-center md:justify-between'>
+            <div className='hidden md:block'>
+                <Link className='font-sans font-bold text-xl text-gray-600' to={'/'}> Tech Review</Link>
+            </div>
+            <div className='flex gap-4 text-xl'>
+                <CustomLink to={'/Homepage'}>Home</CustomLink>
+                <CustomLink to={'/Reviews'}>Reviews</CustomLink>
+                <CustomLink to={'/DashBoard'}>DashBoard</CustomLink>
+                <CustomLink to={'/Blogs'}>Blogs</CustomLink>
+                <CustomLink to={'/About'}>About</CustomLink>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
